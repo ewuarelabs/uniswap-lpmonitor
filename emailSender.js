9e9a4d467778b3c95ad7a1765a069c0d3859c2e0
Jelilat Anofiu <anofiujelailah@gmail.com>
2021-09-21 12:50:45 +0100

    Update emailSender.js

--- emailSender.js
+++ emailSender.js
@@ -4,12 +4,13 @@ const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
 
 sendgrid.setApiKey(SENDGRID_API_KEY)
 
-export const sendEmail = (subject, message) => {
+export const sendEmail = (subject, message, htmlMessage) => {
     const msg = {
         to: process.env.to, // your recipient
         from: process.env.from, // your verified sender
         subject: subject,
         text: message,
+        html: htmlMessage,
       }
       sendgrid
       .send(msg)
